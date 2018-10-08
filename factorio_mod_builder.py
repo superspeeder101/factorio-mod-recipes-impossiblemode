391fb96ea3e91d37dc1ff75c7dcef6de940e0038
superspeeder101 <36306837+superspeeder101@users.noreply.github.com>
2018-10-08 10:06:52 -0700

    Update factorio_mod_builder.py

--- factorio_mod_builder.py
+++ factorio_mod_builder.py
@@ -2,6 +2,13 @@
 class CMTool:
   def __init__(self, prompt):
     self.prompt = prompt
+  
+  def __call__(self):
+    # SETUP
+    should_still_run = True
+    
+    while should_still_run:
+      pass
 
 class Recipe: pass
 class Item: pass
